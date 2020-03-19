1119189bdbbaf91f27c6e28b2b5a6a31081bed7f
Nathan-Binkley <nbink2@gmail.com>
2020-03-19 01:02:14 -0400

    Database online

--- LocTest.py
+++ LocTest.py
@@ -2,6 +2,7 @@ import datetime
 import io
 import json
 import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import random
 import sys
 import time
@@ -25,8 +26,9 @@ geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 auth = tweepy.OAuthHandler(keys.API_KEY[0], keys.API_KEY[1])
 auth.set_access_token(keys.ACCESS_TOKEN[0],keys.ACCESS_TOKEN[1])
 word = tweepy.API(auth)
-cities = ['Greenville, SC', 'Washington,DC', 'Tampa, FL', 'Moscow, Russia']
+cities = []#'Greenville, SC', 'Washington,DC', 'Tampa, FL', 'Moscow, Russia']
 locations = {} #json formatter plz yes
+location_coords = []
 
 dist_mult = [720, 360] #used later for drawing on the map
 
@@ -36,6 +38,7 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 PURPLE = (255, 0, 255)
 WHITE = (255,255,255)
+BLACK = (0,0,0)
 
 
 class MyStreamListener(tweepy.StreamListener):
@@ -59,19 +62,51 @@ class MyStreamListener(tweepy.StreamListener):
             print(status_code)
 
 def proc_loc():
-    
-    
-    location_coords = []
+
+    pass_in_json() #curr database of locations
     print(cities)
+    for city in cities:
+        if len(cities) > 0:
+            #print(city)
+            if city:
+                try:
+                    if city in locations:
+                        #print("city in locations")
+                        location_coords.append((locations[str(city)]['lat'],locations[str(city)]['long']))
+                        
+                    else:
+                        #print("City not in locations")
+                        format_json(locations, str(city), None, None) #prepare for non-existant city
+                        l = geolocator.geocode(city)
+                        if l.latitude and l.longitude:
+                            location_coords.append((l.latitude, l.longitude))
+
+                        if l.latitude: #if city DOES infact exist, overwrite
+                            del locations[city]
+                            format_json(locations, str(city), l.latitude, l.longitude)
+
+                except Exception as e:
+                    print(e)
+
+    write_json(locations)
+
+
+def draw_loc():
+    locs = []
     pygame.init()
     os.environ['SDL_VIDEO_WINDOW_POS'] = "50,50"
     win = pygame.display.set_mode((1440, 720))    
     fps = 60
     fpsClock = pygame.time.Clock()  
 
+    mapImg = pygame.image.load("img/map.jpg")
+    mapImg = pygame.transform.scale(mapImg, (1440,720))
+
 
     while True:
         win.fill((0, 0, 0))
+        win.blit(mapImg,(0,0))
+        
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -79,42 +114,20 @@ def proc_loc():
                 sys.exit()
         
         # Update.
-        if len(cities) > 0:
-            city = cities.pop()
-            
-            if city:
-                try:
-                    if city in locations:
-                        print("city in locations")
-                        location_coords.append((locations[str(city)]['lat'],locations[str(city)]['long']))
-                        print("location added to json")
-                        time.sleep(.5)
-                    else:
-                        print("city not in locations")
-                        l = geolocator.geocode(city)
-                        location_coords.append((l.latitude, l.longitude))
-                        format_json(locations, str(city), l.latitude, l.longitude)
-                        print("Formatted Json")
-                    
-                except Exception as e:
-                    print(e)
-                    
+        if len(location_coords) > 0:
+            locs.append(location_coords.pop())
+            time.sleep(.2)
         else:
-            pygame.draw.rect(win,WHITE,(0,0,4,4))    
+            pygame.draw.rect(win,BLACK,(0,0,4,4))    #locations are processed
                 
+        
+        for location in locs:
+            if location[0] and location[1]:
+                pos_y = (((720/2) + 4*location[0]) * -1) + 720
+                pos_x = (1440/2) + 4*location[1] 
                 
-        
-        
-        
-        
-        for location in location_coords:
-            pos_y = (((720/2) + 4*location[0]) * -1) + 720
-            pos_x = (1440/2) + 4*location[1] 
             
-            
-            pygame.draw.rect(win,YELLOW,(pos_x,pos_y,4,4))
-
-        write_json(locations)
+            pygame.draw.rect(win,RED,(pos_x-2,pos_y-2,4,4))
         
         pygame.display.flip()
         fpsClock.tick(fps)
@@ -127,12 +140,11 @@ def launch_stream():
     myStream.filter(follow = ['25073877'], is_async = False) #Best solution.
     
 
-def write_json(data): # format of 
-                                 # { cities: [ {cityname: cityname, lat: lat, long: long},...] }
-    with io.open("locations.json", "w") as f:
+def write_json(data): # format of "{ cities: [ {cityname: cityname, lat: lat, long: long},...] }"
+    with io.open("locations.json", "w+") as f:
         json.dump(data, f, indent = 4)
 
-def format_json(data, city, lat, lon):
+def format_json(data, city, lat, lon): #add to list to be written to the json in write_json
     data[city] = []
     data[city] = {
         "lat" : lat,
@@ -144,19 +156,17 @@ def pass_in_json(): # Return list (city name, lat, long) ???
     try:
         with open("locations.json", "r") as f:
             locations = json.loads(f.read())
-    except:
+    except: #empty list
         pass
     
 
-pass_in_json()
 
 
-
-#launch_stream()
+launch_stream()
 
 proc_loc()
 
-
+draw_loc()
 
 
 
